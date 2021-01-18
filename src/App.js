614711d16e5f8ded36490c2a88117a8629245449
Ricardo <rik00972@gmail.com>
2021-01-18 02:27:54 -0300

    add useEffect+last changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 import Frase from "./components/Frase";
 
@@ -23,6 +23,12 @@ const Boton = styled.button`
 	padding: 1rem 3rem;
 	font-size: 2rem;
 	border: 2px solid black;
+	transition: background-size 0.8s ease;
+
+	:hover {
+		cursor: pointer;
+		background-size: 400px;
+	}
 `;
 
 function App() {
@@ -43,6 +49,10 @@ function App() {
 	// 	const frase = api.then((respuesta) => respuesta.json());
 	// 	frase.then((resultado) => console.log(resultado));
 	// };
+
+	useEffect(() => {
+		consultarApi();
+	}, []);
 	return (
 		<Contenedor>
 			<Frase frase={frase} />
