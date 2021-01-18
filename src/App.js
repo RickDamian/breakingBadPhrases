4ab7ff115e3d5020c10e732fc93a9a49e45cefd6
Ricardo <rik00972@gmail.com>
2021-01-18 02:18:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+import Frase from "./components/Frase";
+
+const Contenedor = styled.div`
+	display: flex;
+	align-items: center;
+	padding-top: 5rem;
+	flex-direction: column;
+`;
+
+const Boton = styled.button`
+	background: -webkit-linear-gradient(
+		top left,
+		#007d35 0%,
+		#007d35 40%,
+		#0f574e 100%
+	);
+	font-family: Arial, Helvetica, sans-serif;
+	background-size: 300px;
+	color: #fff;
+	margin-top: 3rem;
+	padding: 1rem 3rem;
+	font-size: 2rem;
+	border: 2px solid black;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//state de frases
+	const [frase, guardarFrase] = useState({});
+
+	// consutlar api con async/await
+	const consultarApi = async () => {
+		const api = await fetch(
+			"https://breaking-bad-quotes.herokuapp.com/v1/quotes"
+		);
+		const frase = await api.json();
+		guardarFrase(frase[0]);
+	};
+	// Consutlar api con fetch
+	// const consultarApi = () => {
+	// 	const api = fetch("https://breaking-bad-quotes.herokuapp.com/v1/quotes");
+	// 	const frase = api.then((respuesta) => respuesta.json());
+	// 	frase.then((resultado) => console.log(resultado));
+	// };
+	return (
+		<Contenedor>
+			<Frase frase={frase} />
+			<Boton onClick={consultarApi}>Obtener Frase</Boton>
+		</Contenedor>
+	);
 }
 
 export default App;
